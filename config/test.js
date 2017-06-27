ace897054b47fc15f29429ed1c863e8297a960f7
Enrique Vidal <enrique@cloverinteractive.com>
2017-06-26 20:58:03 -0700

    Add webpack test config

---
+++ config/test.js
@@ -0,0 +1,73 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  devtool: 'inline-cheap-module-source-map',
+
+  entry: {
+    base: path.join(__dirname, '..', 'client', 'index'),
+    test: path.join(__dirname, '..', 'spec'),
+  },
+
+  output: {
+    path: path.join(__dirname, '..', 'build'),
+    filename: 'test.js',
+    publicPath: '/',
+  },
+
+  resolve: {
+    extensions: ['.css', '.elm', '.js', '.jsx', '.json'],
+    modules: [
+      path.join(__dirname, '..', 'client'),
+      path.join(__dirname, '..', 'node_modules'),
+    ],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.elm$/,
+        exclude: [
+          /elm-stuff/,
+          /node_modules/,
+        ],
+        use: {
+          loader: 'elm-webpack-loader',
+          options: {},
+        },
+      },
+      {
+        test: /\.jsx?/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader', options: { sourceMap: true } },
+          { loader: 'resolve-url-loader' },
+        ],
+      },
+    ],
+  },
+
+  externals: {
+    cheerio: 'window',
+    'react/addons': true,
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('test'),
+      },
+    }),
+  ],
+
+  node: {
+    fs: 'empty',
+  },
+}
