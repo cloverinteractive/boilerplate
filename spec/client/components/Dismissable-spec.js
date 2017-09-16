73bcbda096bba2bf1e39af3ea2f9884fcdbb73c8
Enrique Vidal <enrique@cloverinteractive.com>
2017-09-16 09:15:27 -0700

    Add <Dismissable /> spec

---
+++ spec/client/components/Dismissable-spec.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import sinon from 'sinon';
+import Dismissable from 'components/Dismissable';
+
+const dismiss = sinon.spy();
+
+describe('<Dismissable />', () => {
+  it('renders', () => {
+    const wrapper = mount(
+      <Dismissable dismiss={dismiss}>
+        <h1>Dismissable Heading</h1>
+      </Dismissable>
+    );
+
+    const heading = wrapper.find('h1');
+    expect(heading.text()).to.eql('Dismissable Heading');
+  })
+
+  it('dismisses itself', (done) => {
+    const wrapper = mount(
+      <Dismissable dismiss={dismiss} timeout={10}>
+        <h1>Dismissable Heading</h1>
+      </Dismissable>
+    );
+
+    setTimeout(() => {
+      expect(dismiss.callCount).to.eql(1);
+      done();
+    }, 10);
+  });
+});
