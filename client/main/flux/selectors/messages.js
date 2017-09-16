da94fb360f0f9e61fefc1d4833b0160668ae87aa
Enrique Vidal <enrique@cloverinteractive.com>
2017-09-16 00:31:29 -0700

    Add missing annotations and remove mocha warnings

--- client/main/flux/selectors/messages.js
+++ client/main/flux/selectors/messages.js
@@ -1,2 +1,10 @@
-export default ({ messages: alerts }) => (
+// @flow
+
+import type { Message, State } from 'main/constants/types';
+
+type Store = {
+  messages: State,
+};
+
+export default ({ messages: alerts }: Store): Array<Message> => (
   alerts.ids.map(id => alerts.entities[id]));
