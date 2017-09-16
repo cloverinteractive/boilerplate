ac138159d7c60253d95512abd32327fb23ad6478
Enrique Vidal <enrique@cloverinteractive.com>
2017-09-16 10:08:36 -0700

    Add <Header /> spec

---
+++ spec/client/main/components/Header-spec.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount, shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import rootReducer from 'root-reducer';
+
+import Header from 'main/components/Header';
+import { Message } from 'semantic-ui-react';
+import Messages from 'main/components/Messages';
+import Nav from 'main/components/Nav';
+import { addError, addWarning } from 'main/flux/actions/messages';
+
+const initialState = {
+  main: {
+    messages: {
+      entities: {},
+      ids: [],
+    },
+  },
+};
+
+const middleware = applyMiddleware(thunkMiddleware)
+const store = createStore(rootReducer, initialState, middleware)
+let wrapper;
+let component;
+
+context('Main', () => {
+  before(() => {
+    wrapper = mount(
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    );
+
+    component = wrapper.find('Header');
+  });
+
+  describe('<Header />', () => {
+    it('renders', () => {
+      expect(component).to.exist;
+    });
+
+    describe('children', () => {
+      it('has a <Nav />', () => {
+        const subject = wrapper.find(Nav);
+        expect(subject).to.have.length(1);
+      });
+
+      it('has <Messages />', () => {
+        const subject = wrapper.find(Messages);
+        expect(subject).to.have.length(1);
+      });
+    });
+  });
+
+  describe('Flux', () => {
+    it('Renders messages', (done) => {
+      expect(wrapper.find(Message)).to.have.length(0);
+      store.dispatch(addError('Some Error'));
+
+      setTimeout(() => {
+        expect(wrapper.find(Message)).to.have.length(1);
+        done();
+      }, 10);
+    });
+  });
+});
