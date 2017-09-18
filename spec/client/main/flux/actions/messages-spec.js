aed5b2c95462a638af7b49ac4a65852e2c142e03
Enrique Vidal <enrique@cloverinteractive.com>
2017-09-17 22:13:32 -0700

    Add addNotice action spec

--- spec/client/main/flux/actions/messages-spec.js
+++ spec/client/main/flux/actions/messages-spec.js
@@ -2,7 +2,7 @@ import last from 'lodash/last';
 import { expect } from 'chai';
 import configureStore from 'redux-mock-store';
 import thunkMiddleware from 'redux-thunk';
-import removeMessage, { addError, addWarning } from 'main/flux/actions/messages';
+import removeMessage, { addNotice, addError, addWarning } from 'main/flux/actions/messages';
 import * as types from 'main/constants/action-types';
 
 const middlewares = [thunkMiddleware];
@@ -28,6 +28,19 @@ context('Main', () => {
       const store = mockStore(getState)
 
       Promise.resolve(store.dispatch(addWarning('Warning')))
+      .then(() => last(store.getActions()))
+      .then((action) => {
+        expect(action).to.have.property('type', types.ADD_MESSAGE);
+        expect(action).to.have.property('message');
+      })
+      .then(done)
+      .catch(done)
+    });
+
+    it('adds notice', (done) => {
+      const store = mockStore(getState)
+
+      Promise.resolve(store.dispatch(addNotice('Notice')))
       .then(() => last(store.getActions()))
       .then((action) => {
         expect(action).to.have.property('type', types.ADD_MESSAGE);
