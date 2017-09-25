402c3f758f096fde86c70983c35e1ef42ac5e6a3
Enrique Vidal <enrique@cloverinteractive.com>
2017-09-25 08:02:20 -0700

    Add context to Libraries-spec.jsx

--- spec/client/pages/components/Libraries-spec.js
+++ spec/client/pages/components/Libraries-spec.js
@@ -7,33 +7,35 @@ import Libraries, { Library } from 'pages/components/Libraries';
 
 let wrapper;
 
-describe('Pages', () => {
-  before(() => {
-    wrapper = mount(
-      <Libraries libraries={libraries} />
-    )
-  });
-
-  it('renders', () => {
-    expect(wrapper).to.exist;
-  });
-
-  describe('children', () => {
-    it('is a <ul />', () => {
-      expect(wrapper.find('ul')).to.have.length(1);
+context('Pages', () => {
+  describe('<Libraries />', () => {
+    before(() => {
+      wrapper = mount(
+        <Libraries libraries={libraries} />
+      )
     });
 
-    it('has many <Library />', () => {
-      expect(wrapper.find(Library)).to.have.length(libraries.length);
-      expect(wrapper.find('li')).to.have.length(libraries.length);
+    it('renders', () => {
+      expect(wrapper).to.exist;
     });
 
-    libraries.map((library, position) => {
-      it(`has a link for ${library.library} - ${library.description}`, () => {
-        const li = wrapper.find('li');
+    describe('children', () => {
+      it('is a <ul />', () => {
+        expect(wrapper.find('ul')).to.have.length(1);
+      });
 
-        expect(li.at(position).find('a').text()).to.eql(library.library);
-        expect(li.at(position).find('em').text()).to.eql(library.description);
+      it('has many <Library />', () => {
+        expect(wrapper.find(Library)).to.have.length(libraries.length);
+        expect(wrapper.find('li')).to.have.length(libraries.length);
+      });
+
+      libraries.map((library, position) => {
+        it(`has a link for ${library.library} - ${library.description}`, () => {
+          const li = wrapper.find('li');
+
+          expect(li.at(position).find('a').text()).to.eql(library.library);
+          expect(li.at(position).find('em').text()).to.eql(library.description);
+        });
       });
     });
   });
