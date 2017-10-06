85f0d00dbcdcf317a53e7c1f21131b2b1564794b
Enrique Vidal <enrique@cloverinteractive.com>
2017-10-06 16:16:22 -0700

    Upgrade to React 16 (#7)

--- spec/browser.js
+++ spec/browser.js
@@ -1,5 +1,9 @@
 import jsdom from 'jsdom';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import 'ignore-styles';
+
+configure({ adapter: new Adapter() });
 
 const document = new jsdom.jsdom('<!doctype html><html><body></body>body></html>html>');
 const window = document.defaultView;
