484a6cef1b95934aa9cfd4333dc43af3dcc542b1
Enrique Vidal <enrique@cloverinteractive.com>
2020-04-27 12:37:29 -0700

    Fix Express status codes (#31)
    * Don't just respond with 200 and allow components to prefetch
    
    * Simplify loadData callbacks
    
    * Bump minor version

--- src/server/helpers/renderer.js
+++ src/server/helpers/renderer.js
@@ -1,10 +1,12 @@
 import { renderToString } from 'react-dom/server';
 import { Helmet } from 'react-helmet';
 import serialize from 'serialize-javascript';
+import { isDevelopment } from './env-access';
 
 export default (Component, store) => {
   const content = renderToString(Component);
   const helmet = Helmet.renderStatic();
+  const staticStyles = isDevelopment ? '' : '<link rel="stylesheet" href="/styles.css">';
 
   const template = `<!doctype html>
     <html lang="en-US" class="has-navbar-fixed-top">
@@ -14,7 +16,7 @@ export default (Component, store) => {
         ${helmet.link.toString()}
         ${helmet.title.toString()}
         ${helmet.meta.toString()}
-        <link rel="stylesheet" href="/styles.css">
+        ${staticStyles}
         <script src="/vendors.js"></script>
       </head>
       <body>
--- src/server/routes.jsx
+++ src/server/routes.jsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import Routes from 'routes';
-import { StaticRouter } from 'react-router-dom';
+import Routes, { routes } from 'routes';
 import { Provider } from 'react-redux';
-import render from 'server/helpers/renderer';
-import store from 'server/store';
+import { StaticRouter, Route, matchPath } from 'react-router-dom';
+import render from './helpers/renderer';
+import store from './store';
+import Error500 from '../pages/components/Error500';
 
 const SSR = ({ context, location }) => (
   <Provider store={store}>
@@ -13,7 +14,40 @@ const SSR = ({ context, location }) => (
   </Provider>
 );
 
-export default (req, res) => {
-  const context = {};
-  res.send(render(<SSR context={context} location={req.url} />, store));
+/* eslint-disable no-console */
+export const errorHandler = (error, _, res) => {
+  console.error(error);
+
+  res
+    .status(500)
+    .send(
+      render(
+        <SSR context={{}} location="/error500">
+          <Route>
+            <Error500 />
+          </Route>
+        </SSR>,
+        store,
+      ),
+    );
+};
+/* eslint-disable no-console */
+
+export default
+async (req, res, next) => {
+  try {
+    const activeRoute = routes.find((route) => matchPath(req.url, route)) || {};
+
+    const context = await activeRoute.loadData
+      ? activeRoute.loadData(req.path)
+      : Promise.resolve();
+
+    const status = !activeRoute.path ? 404 : 200;
+
+    res
+      .status(status)
+      .send(
+        render(<SSR context={context} location={req.url} />, store),
+      );
+  } catch (err) { next(err); }
 };
